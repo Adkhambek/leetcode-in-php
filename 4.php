011cadbb41096d4ce408e0be069c0fcc9ba1891c
Adham <muzaffarov.adham@gmail.com>
2022-06-16 14:51:53 +0500

    third maximum number

--- 4.php
+++ 4.php
@@ -1,1 +1,11 @@
 <?php
+#https://leetcode.com/problems/third-maximum-number/
+function thirdMax($nums) {
+	if(count($nums) < 3) return max($nums);
+	 $unique_num = array_unique($nums);
+	if(count($unique_num) < 3) return max($nums);
+	rsort($unique_num);
+	return $unique_num[2];
+}
+
+print_r(thirdMax([3,2,1,3,2]));
