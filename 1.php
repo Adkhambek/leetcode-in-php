f9b6883c4c8f8347c8389b7be3d8872b64251d49
Adham <muzaffarov.adham@gmail.com>
2022-06-16 12:05:56 +0500

    two sum

--- 1.php
+++ 1.php
@@ -1,1 +1,26 @@
 <?php
+# https://leetcode.com/problems/two-sum/
+
+//function twoSum($nums, $target) {
+//	for($i = 0; $i < count($nums); $i++) {
+//		for($k = $i + 1; $k < count($nums); $k++) {
+//			if($nums[$k] == $target - $nums[$i]) {
+//				return [$i, $k];
+//			}
+//		}
+//	}
+//}
+
+function twoSum($nums, $target) {
+	foreach ($nums  as  $key => $first_num) {
+		$second_num = $target - $first_num;
+		if(in_array($second_num, $nums)) {
+			if(array_search($second_num, $nums) != $key) {
+				return [$key, array_search($second_num, $nums)];
+			}
+
+		}
+	}
+}
+
+print_r(twoSum([3,2,4], 6));
