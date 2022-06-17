334a0a5c9d5cfbe39cf86a7c2521aeacd1355242
Adham <muzaffarov.adham@gmail.com>
2022-06-17 11:37:27 +0500

    search-insert-position

--- 10.php
+++ 10.php
@@ -1,1 +1,12 @@
 <?php
+# https://leetcode.com/problems/search-insert-position/
+
+function searchInsert($nums, $target) {
+	for ($i = 0; $i <= count($nums); $i++){
+		if($nums[$i] == $target) return $i;
+	}
+}
+
+var_dump(searchInsert([1,3,5,6], 5));
+
+// TODO: not done yet
