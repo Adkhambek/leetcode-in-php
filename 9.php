95533b388d478443b709b01276300d499ea47dc8
Adham <muzaffarov.adham@gmail.com>
2022-06-17 11:36:45 +0500

    remove-element

--- 9.php
+++ 9.php
@@ -1,1 +1,12 @@
 <?php
+# https://leetcode.com/problems/remove-element/
+
+function removeElement($nums, $val) {
+	$count = count($nums);
+	for ($i = 0; $i < $count; $i++) {
+		if($nums[$i] === $val) unset($nums[$i]);
+	}
+	return count($nums);
+}
+
+var_dump(removeElement([0,1,2,2,3,0,4,2], 2));
