8fa3df5a3a3ac1ee2a233139c9f116d914bfdfb1
Adham <muzaffarov.adham@gmail.com>
2022-06-17 11:38:13 +0500

    length-of-last-word

--- 11.php
+++ 11.php
@@ -1,1 +1,9 @@
 <?php
+# https://leetcode.com/problems/length-of-last-word/
+
+function lengthOfLastWord($s) {
+	$arr = explode(" ", trim($s));
+	return strlen($arr[count($arr) - 1]);
+}
+
+lengthOfLastWord("   fly me   to   the moon  ");
