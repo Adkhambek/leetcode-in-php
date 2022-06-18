e080464071f035fba0202e08405c6ca5034e133d
Adham <muzaffarov.adham@gmail.com>
2022-06-18 16:44:28 +0500

    add digits

--- 18.php
+++ 18.php
@@ -1,1 +1,17 @@
 <?php
+# https://leetcode.com/problems/add-digits/
+
+function addDigits($num) {
+	while($num > 9) {
+		$sum = 0;
+		while($num > 0) {
+			$sum += $num % 10;
+			$num = floor($num / 10);
+		}
+
+		$num = $sum;
+	}
+	return $num;
+}
+
+echo addDigits(38);
