bf45e96a26fe64d91ff55aeaffe38b26ea2e027a
Adham <muzaffarov.adham@gmail.com>
2022-06-18 16:44:53 +0500

    ugly number

--- 19.php
+++ 19.php
@@ -1,1 +1,17 @@
 <?php
+# https://leetcode.com/problems/ugly-number/
+
+function isUgly($n) {
+	if($n <= 0) return false;
+	while ($n > 1) {
+		if($n % 2 === 0) $n /= 2;
+		else if($n % 3 === 0) $n /= 3;
+		else if($n % 5 === 0) $n /= 5;
+		else return false;
+	}
+	return true;
+}
+
+var_dump(isUgly(6))."<br>";
+var_dump(isUgly(1))."<br>";
+var_dump(isUgly(14))."<br>";
