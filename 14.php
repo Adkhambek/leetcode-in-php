5474fb5e784ad41e8aaab649f42d4abf14f24c7b
Adham <muzaffarov.adham@gmail.com>
2022-06-18 16:42:57 +0500

    single number

--- 14.php
+++ 14.php
@@ -1,1 +1,17 @@
 <?php
+# https://leetcode.com/problems/single-number/
+
+function singleNumber($nums) {
+	$map = [];
+	foreach ($nums as $num) {
+		$map[$num] = 0;
+	}
+	foreach ($nums as $num) {
+		if(array_key_exists($num, $map)) $map[$num]++;
+	}
+	foreach (array_keys($map) as $key) {
+		if($map[$key] === 1) return $key;
+	}
+}
+
+echo singleNumber([1]);
