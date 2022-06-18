abb1f70095066a5d270932f56cd1bc51e4919819
Adham <muzaffarov.adham@gmail.com>
2022-06-18 16:42:33 +0500

    climbing stairs

--- 13.php
+++ 13.php
@@ -1,1 +1,15 @@
 <?php
+# https://leetcode.com/problems/climbing-stairs/
+
+function climbStairs($n) {
+	$a = 1;
+	$b = 1;
+	while($n > 0) {
+		$b += $a;
+		$a = $b - $a;
+		$n--;
+	}
+	return $a;
+}
+
+echo climbStairs(5);
